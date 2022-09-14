5b0f22d00b909a9c8d07e31ba1fa739a74f409d5
Renan Araujo <renanaraujo852@hotmail.com>
2022-09-14 00:01:24 -0300

    basic config api

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+import app from "./app";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Servidor is running on port ${PORT}`));
