af8053adbcf2db6c1784cf7ecee45d2bc8c43f26
Renan Araujo <renanaraujo852@hotmail.com>
2022-09-15 16:51:11 -0300

    feat: create validate schema middleware

---
+++ src/middlewares/validateSchemaMiddleware.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+import {ObjectSchema} from "joi"
+
+export default function validateSchema(schema: ObjectSchema) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const { error } = schema.validate(req.body, { abortEarly: false });
+
+        if (error) throw {type: "unprocessableEntity", message: error.message};
+
+        next();
+    };
+};
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,9 +1,11 @@
+import { signUpSchema, signInSchema } from './../schemas/authSchema';
 import { Router } from "express";
 import { signInUser, signUpUser } from "../controllers/authController";
+import validateSchema from "../middlewares/validateSchemaMiddleware";
 
 const authRouter = Router();
 
-authRouter.post("/signup", signUpUser);
-authRouter.post("/signin", signInUser);
+authRouter.post("/signup", validateSchema(signUpSchema), signUpUser);
+authRouter.post("/signin", validateSchema(signInSchema), signInUser);
 
 export default authRouter;
