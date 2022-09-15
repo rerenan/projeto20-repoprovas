7d10de8a0dc3d7f2233d36b22a1ff719ad6b9849
Renan Araujo <renanaraujo852@hotmail.com>
2022-09-14 22:49:31 -0300

    add tests table to database

---
+++ prisma/migrations/20220915014913_add_tests_table/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "tests" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "pdfUrl" TEXT NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+    "teacherDisciplineId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "tests_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "tests" ADD CONSTRAINT "tests_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tests" ADD CONSTRAINT "tests_teacherDisciplineId_fkey" FOREIGN KEY ("teacherDisciplineId") REFERENCES "teachersDisciplines"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
