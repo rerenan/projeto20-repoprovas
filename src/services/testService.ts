d63249765477ff705ae955afd1ef7b1ad7bad3da
Renan Araujo <renanaraujo852@hotmail.com>
2022-09-18 18:58:45 -0300

    feat: create test service basic structure

--- src/services/testService.ts
+++ src/services/testService.ts
@@ -1,6 +1,14 @@
 import * as testRepository from "../repositories/testRepository"
-import { TestInsertData } from "../types/test";
+import { TestReceivedData } from "../types/test";
 
-export async function postTest(testData:TestInsertData) {
+
+export async function postTest(testData:TestReceivedData) {
+    const { 
+        name, 
+        pdfUrl, 
+        category, 
+        discipline, 
+        teacher 
+    } = testData;
     
 };
