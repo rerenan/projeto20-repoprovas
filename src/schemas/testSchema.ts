de34271fc2e7c8426b126804efea9ed6809a992e
Renan Araujo <renanaraujo852@hotmail.com>
2022-09-18 19:56:44 -0300

    fix: set categories name in test schema

--- src/schemas/testSchema.ts
+++ src/schemas/testSchema.ts
@@ -3,7 +3,7 @@ import joi from "joi";
 export const testSchema = joi.object({
     name: joi.string().required(),
     pdfUrl: joi.string().uri().required(),
-    category: joi.string().required(),
+    category: joi.valid("Projeto", "Prática", "Recuperação").required(),
     discipline: joi.string().required(),
     teacher: joi.string().required()
 });
