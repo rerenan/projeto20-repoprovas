8ad6115469caf4f8d1046fe8879ccffa84ae5582
Renan Araujo <renanaraujo852@hotmail.com>
2022-09-19 00:27:31 -0300

    fix: add token middleware in tests routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import {Router} from "express";
+import tokenMiddleware from "../middlewares/tokenMiddleware";
 import testRouter from "./testRouter";
 import userRouter from "./userRouter";
 
 const router = Router();
 
 router.use(userRouter);
-router.use("/test", testRouter);
+router.use("/test",tokenMiddleware, testRouter);
 
 export default router;
