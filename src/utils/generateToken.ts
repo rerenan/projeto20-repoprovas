61d0f7b45a60e172d26186f9fd0f3af9424ce96b
Renan Araujo <renanaraujo852@hotmail.com>
2022-09-19 00:26:12 -0300

    fix: change token expiration time to 10 days

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 
 export default function generateUserToken(userId: number) {
     const secretKey = process.env.JWT_SECRET;
-    const time = 60*60*5 // 5 horas
+    const time = 60*60*24*10 // 10 dias
     
     const token = jwt.sign({userId}, secretKey,{expiresIn: time});
     return token;
