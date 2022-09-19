f0dfeaa85493e901d97d059b95e6154e1b76f3c5
Renan Araujo <renanaraujo852@hotmail.com>
2022-09-18 23:58:10 -0300

    fix: status code to get test set to 200

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -4,18 +4,21 @@ import * as testService from "../services/testService"
 
 export async function createTest(req: Request, res: Response) {
 
-    const test = await testService.postTest(req.body)
+    const test = await testService.postTest(req.body);
+
     res.status(201).send(test);
 }
 
 export async function getByDisciplines(req: Request, res: Response) {
 
     const tests = await testService.getAllByDisciplines();
+
     res.status(200).send(tests);
 }
 
 export async function getByTeachers(req: Request, res: Response) {
 
     const tests = await testService.getAllByTeachers();
+    
     res.status(200).send(tests);
 }
