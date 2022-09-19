2bd647322dcf0786614859d6d6123698ac9779d9
Renan Araujo <renanaraujo852@hotmail.com>
2022-09-19 01:18:41 -0300

    feat: test test - create test successful

--- tests/factories/userFactory.ts
+++ tests/factories/userFactory.ts
@@ -1,6 +1,10 @@
 import { faker } from "@faker-js/faker";
 import client from "../../src/config/db";
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import generateUserToken from "../../src/utils/generateToken";
+
+dotenv.config();
 
 export function userSignUpDataFactory(){
     
@@ -34,4 +38,42 @@ export async function signUpFactory(){
     
     delete user.confirmPassword;
     return user;
+};
+
+export async function testDataFactory() {
+    
+    const categories = await client.categories.findMany();
+    const teachersDisciplines = await client.teachersDisciplines.findMany({
+        select: {
+            teacher: true,
+            discipline: true
+        }
+    })
+
+    const arrayCategories = categories.map ((category)=> category.name);
+    const arrayTeachersDisciplines = teachersDisciplines.map((teacherDisc)=> [teacherDisc.discipline.name, teacherDisc.teacher.name]);
+    const randomTeacherDisciplines = faker.helpers.arrayElement(arrayTeachersDisciplines);
+    return {
+        name: faker.random.words(3),
+        pdfUrl: faker.internet.url(),
+        category: faker.helpers.arrayElement(arrayCategories),
+        discipline: randomTeacherDisciplines[0],
+        teacher: randomTeacherDisciplines[1]
+    }
+}
+
+export async function signInFactory(){
+    const user = userSignUpDataFactory();
+    
+    const result = await client.users.create({
+        data: {
+            email: user.email,
+            password: bcrypt.hashSync(user.password,10)
+        }
+    });
+    
+    const token = generateUserToken(result.id);
+
+    return token;
+    
 };
--- tests/test.test.ts
+++ tests/test.test.ts
@@ -3,6 +3,7 @@ import supertest from "supertest";
 
 import client from "../src/config/db";
 import app from "../src/app";
+import { signInFactory, testDataFactory } from "./factories/userFactory";
 
 dotenv.config();
 
@@ -17,15 +18,26 @@ beforeEach(async () => {
   });
 
 describe("Test POST /test/create", () => {
-    it.todo("Shold return status 201, if registered and return test");
-    it.todo("Should return status 422, if send test in format invalid")
-    it.todo("Should return status 401, if authentication token is invalid")
+    it("Shold return status 201, if registered and return test", async () => {
+        
+        const test = await testDataFactory();
+        console.log(test)
+        const token = await signInFactory();
+
+        const result = await supertest(app).post("/test/create").send(test).set("Authorization", `Bearer ${token}`);
+
+        expect(result.status).toEqual(201);
+        expect(result.body).not.toBeNull();
+    });
+    it.todo("Should return status 401, if authentication token is invalid");
+    it.todo("Should return status 404, if fields relations is incompatible");
+    it.todo("Should return status 422, if send test in format invalid");
 })
 describe("Test GET /test/bydisciplines", () => {
   
-    it.todo("Should return status 401, if authentication token is invalid")
+    it.todo("Should return status 401, if authentication token is invalid");
 })
 describe("Test GET /test/byteachers", () => {
     
-    it.todo("Should return status 401, if authentication token is invalid")
+    it.todo("Should return status 401, if authentication token is invalid");
 })
